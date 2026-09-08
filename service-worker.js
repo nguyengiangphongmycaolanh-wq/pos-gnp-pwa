f1a9ca60bf8b1d31eddfd4d845bff3baafedaf1b
nguyengiangphongmycaolanh-wq <nguyengiangphongmycaolanh@gmail.com>
2026-09-08 14:21:10 +0700

    Implement service worker for caching resources

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'pos-gnp-shell-v1';
+const SHELL_FILES = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (new URL(req.url).origin !== self.location.origin) return;
+  event.respondWith(
+    fetch(req)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, resClone));
+        return res;
+      })
+      .catch(() => caches.match(req).then((res) => res || caches.match('./index.html')))
+  );
+});
